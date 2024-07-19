da761d130a2eb3565715578173b58268feeb5eab
setografi <hitonaashbell@gmail.com>
2024-07-19 23:03:14 +0700

    dwt & qim

---
+++ dwt&qim/dwt.go
@@ -0,0 +1,17 @@
+package dwt
+
+import (
+    "github.com/gonum/matrix/mat64"
+)
+
+func Transform(signal []float64) []float64 {
+    // Implement DWT here
+    // Placeholder: Return signal as is
+    return signal
+}
+
+func Reconstruct(coeffs []float64) []float64 {
+    // Implement Inverse DWT here
+    // Placeholder: Return coefficients as is
+    return coeffs
+}
---
+++ dwt&qim/qim.go
@@ -0,0 +1,7 @@
+package qim
+
+func ModifyCoefficients(coeffs []float64, message string) []float64 {
+    // Implement QIM modification here
+    // Placeholder: Return coefficients as is
+    return coeffs
+}
